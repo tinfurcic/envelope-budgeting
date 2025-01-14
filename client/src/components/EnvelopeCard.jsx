649d216d5a20398d37ea64781a87bbd1e87f8c7e
Tin Furčić <tin.furcic@gmail.com>
2025-01-14 17:46:26 +0100

    EnvelopeCard font resizing

--- client/src/components/EnvelopeCard.jsx
+++ client/src/components/EnvelopeCard.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const EnvelopeCard = ({ envelope, fetchEnvelopes }) => {
+  const containerRef = useRef(null);
   const [name, setName] = useState(envelope.name);
   const [budget, setBudget] = useState(envelope.budget);
   const [currentAmount, setCurrentAmount] = useState(envelope.currentAmount);
 
   const currency = "$"; // this should be a global setting, somewhere
 
+  useEffect(() => {
+    const updateFontSize = () => {
+      if (containerRef.current) {
+        const { height } = containerRef.current.getBoundingClientRect();
+        containerRef.current.style.setProperty(
+          "--font-size",
+          `${height * 0.01333}rem`,
+        );
+      }
+    };
+
+    window.addEventListener("resize", updateFontSize);
+    updateFontSize();
+
+    return () => window.removeEventListener("resize", updateFontSize);
+  }, []);
+
   return (
-    <div className="envelope-card">
+    <div className="envelope-card" ref={containerRef}>
       <span className="envelope-card__name">{name}</span>
       <div className="envelope-card__amount-left">
         <span className="envelope-card__amount-left--absolute">
