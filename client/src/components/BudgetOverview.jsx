6c6650bd5de8aed9e113b53b952f61901effdcc7
Tin Furčić <tin.furcic@gmail.com>
2025-02-02 13:08:02 +0100

    Refactoring Home

---
+++ client/src/components/BudgetOverview.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import ProgressBar from "./ProgressBar";
+import chartIcon from "../media/chart.png";
+
+const BudgetOverview = () => {
+
+  const { totalBudget, totalCurrentAmount } = useOutletContext();
+
+  const [percentage, setPercentage] = useState(
+    Math.round((totalCurrentAmount * 100) / totalBudget),
+  );
+
+  useEffect(() => {
+    setPercentage(Math.round((totalCurrentAmount * 100) / totalBudget));
+  }, [totalCurrentAmount, totalBudget]);
+
+  return (
+    <div className="budget-overview">
+      <h2 className="budget-overview__heading">Budget overview</h2>
+      <ProgressBar totalBudget={totalBudget} percentage={percentage} />
+      <p className="legend-item">
+        <span className="legend-square"></span> Money left in my envelopes
+      </p>
+      <p className="progress-bar-comparison">
+        <img
+          src={chartIcon}
+          alt="comparison"
+          className="progress-bar-comparison__icon"
+        />
+        That's <span className="bold--green">13% more</span> than usually at this time of the month! (62%) [placeholders]
+        {/* render only if relevant data exists */}
+      </p>
+    </div>
+  );
+};
+
+export default BudgetOverview;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,86 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { useOutletContext } from "react-router-dom";
-import { useAuth } from "./AuthContext";
-import ProgressBar from "./ProgressBar";
-import Button from "./Button";
-import chartIcon from "../media/chart.png";
+import React from "react";
 import useScreenSize from "../hooks/useScreenSize";
+import { useAuth } from "./AuthContext";
+import BudgetOverview from "./BudgetOverview";
+import TodaysExpenses from "./TodaysExpenses";
 
 const Home = () => {
   const { user } = useAuth();
   console.log(user);
-  const {
-    totalBudget,
-    totalCurrentAmount,
-    expenses,
-    loadingData,
-  } = useOutletContext();
   const { isSmall } = useScreenSize();
-  const fakeCurrency = "€";
-
-  const [percentage, setPercentage] = useState(
-    Math.round((totalCurrentAmount * 100) / totalBudget),
-  );
-
-  useEffect(() => {
-    setPercentage(Math.round((totalCurrentAmount * 100) / totalBudget));
-  }, [totalCurrentAmount, totalBudget]);
 
   return (
     <div className="home-page">
       {isSmall && <h1 className="home-page__heading">Home</h1>}
-      <div className="budget-overview">
-        {/* This can be a standalone component */}
-        <h2 className="budget-overview__heading">Budget overview</h2>
-        <ProgressBar totalBudget={totalBudget} percentage={percentage} />
-        <p className="legend-item">
-          <span className="legend-square"></span> Money left in my envelopes
-        </p>
-        <p className="progress-bar-comparison">
-          <img
-            src={chartIcon}
-            alt="comparison"
-            className="progress-bar-comparison__icon"
-          />
-          That's <span className="bold--green">13% more</span> than usually at
-          this time of the month! (62%) [placeholders]
-          {/* render only if relevant data exists */}
-        </p>
-      </div>
-      <div className="latest-expenses">
-        {/* this can be a standalone component */}
-        <div className="latest-expenses__header">
-          <h2 className="latest-expenses__heading">Latest expenses</h2>
-          <Button
-            type="button"
-            className="button"
-            onClick={null}
-            variant="blue"
-            isDisabled={false}
-          >
-            All expenses
-          </Button>
-        </div>
-
-        <div className="latest_expenses__expenses">
-          {loadingData ? (
-            <p>Loading expenses...</p>
-          ) : expenses.length === 0 ? (
-            <p>You don't have any documented expenses yet.</p>
-          ) : (
-            <ul>
-              {expenses.slice(0, 3).map((expense) => {
-                return (
-                  <li key={expense.id}>
-                    {fakeCurrency}
-                    {expense.amount}
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </div>
-      </div>
+      <BudgetOverview />
+      <TodaysExpenses />
       <div className="home-page__this-month-stats">
         <h2 className="home-page__this-month-stats__heading">
           This month's stats
